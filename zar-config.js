b1aab72a37e1c3a8894663889d3eaeee3028dc0e
gendensurennarantsend650-ops <gendensurennarantsend650@gmail.com>
2026-04-01 14:21:54 +0800

    Add files via upload

--- zar-config.js
+++ zar-config.js
@@ -1,24 +1,26 @@
 // zar-config.js
 window.MY_ADS = [
   {
-    afterRowId: 'rowFeatured', 
+    afterRowId: 'rowFeatured', // "Онцлох бүтээлүүд"-ийн доор
     isActive: true,
     label: 'PREMIUM AD',
-    // 2026 оны тренд баннер зураг:
+    // Кино театрын гоё зураг
     image: 'https://img.freepik.com/free-vector/movie-theater-concept-banner-template_23-2148175024.jpg', 
     link: 'https://www.profitablecpmratenetwork.com/d063gyqa?key=d754bdc9c5123dec15f7c0789fdfb1cc'
   },
   {
-    afterRowId: 'rowSeries', 
+    afterRowId: 'rowSeries', // "Цуврал кино"-ны доор
     isActive: true,
     label: 'WATCH NOW',
+    // Стриминг үйлчилгээний гоё зураг
     image: 'https://img.freepik.com/free-vector/streaming-online-app-concept-banner_23-2148530364.jpg',
     link: 'https://www.profitablecpmratenetwork.com/d063gyqa?key=d754bdc9c5123dec15f7c0789fdfb1cc'
   },
   {
-    afterRowId: 'rowInternational', 
+    afterRowId: 'rowInternational', // "Дэлхийн кино"-ны доор
     isActive: true,
     label: 'SPONSORED',
+    // Попкорнтой гоё зураг
     image: 'https://t3.ftcdn.net/jpg/02/60/34/04/360_F_260340451_7Ocl0Uo2Z7vP051U3NfMvVdfBvV2kGk8.jpg',
     link: 'https://www.profitablecpmratenetwork.com/d063gyqa?key=d754bdc9c5123dec15f7c0789fdfb1cc'
   }
